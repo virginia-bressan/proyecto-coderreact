103c6e8e6bafa8d54f5b664b362b7dfe31896bc4
Virginia Bressan <vir.bressan@hotmail.com>
2023-01-30 21:00:40 -0300

    finalizando proyec

--- src/storage/cartContext.jsx
+++ src/storage/cartContext.jsx
@@ -2,28 +2,36 @@ const { createContext, useState } = require("react");
 
 export const cartContext = createContext();
 
-export function CartContextProvider(props){
-    const [cart , setCart] = useState([]);
+export function CartContextProvider(props) {
+    const [cart, setCart] = useState([]);
 
-    function addItem(item){
+    function addItem(item) {
         let newCart = [...cart];
-        const iteminCart = cart.find(iteminCart =>iteminCart.id === item.id)
+        const iteminCart = cart.find(iteminCart => iteminCart.id === item.id)
         newCart.push(item);
         setCart(newCart);
     }
-    
-    function removeItem(id) {
 
+    function removeItem(id) {
+        // Código para buscar y eliminar el item con el id específico
+        const items = [...cart.items];
+        const updatedItems = items.filter(item => item.id !== id);
+        // Actualiza la lista de items con los items restantes después de eliminar el item con el id específico
+        cart.items = updatedItems;
     }
-    
     function getTotalItems() {
         let total = 0;
-        cart.forEach( (item) => total);
+        cart.forEach(item => {
+            total += item.quantity;
+        });
         return total;
+
     }
     return (
-    <cartContext.Provider value={{ cart, addItem, getTotalItems, removeItem }}>
-        {props.children}
-    </cartContext.Provider>
+        <cartContext.Provider value={{ cart, addItem, getTotalItems, removeItem }}>
+            {props.children}
+        </cartContext.Provider>
     );
 }
+
+
